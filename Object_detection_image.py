369908e02fff0602427c6f22575b99105b40d024
topkhung <topkhung@gmail.com>
2018-12-22 11:36:52 +0700

    Add files via upload

---
+++ Object_detection_image.py
@@ -0,0 +1,115 @@
+#Topkhung Edited
+
+# Import packages
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import sys
+
+
+# Import utilites
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+from distutils.version import StrictVersion
+
+# module level variables ##############################################################################################
+# TEST_IMAGE_DIR = os.getcwd() +  "/test_images"
+FROZEN_INFERENCE_GRAPH_LOC = os.getcwd() + "/inference_graph/frozen_inference_graph.pb"
+LABELS_LOC = os.getcwd() + "/" + "label_map.pbtxt"
+NUM_CLASSES = 6
+
+#######################################################################################################################
+
+
+# Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+label_map = label_map_util.load_labelmap(LABELS_LOC)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+# load a (frozen) TensorFlow model into memory
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(FROZEN_INFERENCE_GRAPH_LOC, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    sess = tf.Session(graph=detection_graph)
+
+# Define input and output tensors (i.e. data) for the object detection classifier
+
+# Input tensor is the image
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+# Output tensors are the detection boxes, scores, and classes
+# Each box represents a part of the image where a particular object was detected
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+# Each score represents level of confidence for each of the objects.
+# The score is shown on the result image, together with the class label.
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+# Number of objects detected
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+# Load image using OpenCV and
+# expand image dimensions to have shape: [1, None, None, 3]
+# i.e. a single-column array, where each item in the column has the pixel RGB value
+image = cv2.imread("test.jpg")
+image_expanded = np.expand_dims(image, axis=0)
+
+# Perform the actual detection by running the model with the image as input
+(boxes, scores, classes, num) = sess.run(
+    [detection_boxes, detection_scores, detection_classes, num_detections],
+    feed_dict={image_tensor: image_expanded})
+
+
+# find location coordinate
+height, width, channels = image.shape
+
+# filter boxes with score 0.8
+final_score = np.squeeze(scores)    
+count = 0
+for i in range(100):
+    if scores is None or final_score[i] > 0.80:
+        count = count + 1
+        ymin = boxes[0][i][0]*height
+        xmin = boxes[0][i][1]*width
+        ymax = boxes[0][i][2]*height
+        xmax = boxes[0][i][3]*width
+
+        cv2.rectangle(image,(int(xmin),int(ymin)),(int(xmax),int(ymax)),(255,0,0),3)
+
+
+# Draw the results of the detection (aka 'visulaize the results')
+
+vis_util.visualize_boxes_and_labels_on_image_array(
+    image,
+    np.squeeze(boxes),
+    np.squeeze(classes).astype(np.int32),
+    np.squeeze(scores),
+    category_index,
+    use_normalized_coordinates=True,
+    line_thickness=2,
+    min_score_thresh=0.80)
+
+
+
+print ('Total Sack : ',count)
+print ('Top left : ',xmin,ymin)
+print ('Bottom right : ',xmax,ymax)
+
+# All the results have been drawn on image. Now display the image.
+cv2.imshow('Object detector', image)
+
+# Press any key to close the image
+cv2.waitKey(0)
+
+# Clean up
+cv2.destroyAllWindows()
+
+
